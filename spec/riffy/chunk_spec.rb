65f3b71dbc39d3b4d1cb8aa99a984b539be6659b
Dominic Muller <nicklink483@gmail.com>
2014-08-02 15:22:36 -0700

    clean up tests.  add Chunk specs

---
+++ spec/riffy/chunk_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Riffy::Chunk do
+
+  it "should have an id"
+  it "should have a size in bytes"
+  
+  context "when it's a RIFF chunk" do
+    it "should have an id of RIFF"
+    it "should have a form type"
+    
+  end
+  context "when it's a LIST chunk" do
+    it "should have an id of LIST"
+    it "should have a form type"
+  end
+end
